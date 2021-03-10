c73d66af21f29dacc1f5fe249b2abb82eff627b9
vanzo16 <vanzo16@ya.ru>
2021-03-10 20:40:12 +0300

    add towel task

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
+const arr = [[1, 2, 3], 
+             [4, 5, 6],
+             [7, 8, 9],];
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+function towelSort(arr, d = 1) {
+   return d > 0 ? arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? towelSort(val, d - 1) : val), [])
+                : arr.slice();
+};
+
+console.log(towelSort(arr));
